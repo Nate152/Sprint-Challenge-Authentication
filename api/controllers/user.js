9f522fe5e5bb0685aa913659d2ce921233e5ba5f
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-03-23 13:54:55 -0400

    Answers.md complete, createUser Fx complete

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,10 +3,13 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
+  const newUser = new User({ username, encryptedPassword: password});
+  newUser.save()
+  .then(user => res.status(200).send(user))
+  .catch(err => res.status(500).send(err, res));
+};
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
-};
-
 module.exports = {
   createUser
 };
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -5,24 +5,43 @@ const Schema = mongoose.Schema;
 const SALT_ROUNDS = 11;
 
 const UserSchema = Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  encryptedPassword: {
+    type: String,
+    required: true
+  }
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
 });
 
 UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.encryptedPassword, SALT_ROUNDS, (err, password) => {
+    if (err) return next(err);
+    this.encryptedPassword = password;
+    next();
+  });
+});
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
-});
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
+  bcrypt.compare(plainTextPW, this.encryptedPassword, (err, matchingPassword) => {
+    if (err) return cb(err);
+    cb(null, matchingPassword);
+  });
+};
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-};
 
 // if you're really stuck with this at this point, you can reference this document.
 // https://github.com/LambdaSchool/Auth-JWT/blob/master/models/index.js This is what we're going for here.
